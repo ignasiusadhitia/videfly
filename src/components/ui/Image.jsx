de1a2603081d3e5c2cecf331cc994d346e03c4bf
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-29 00:30:26 +0700

    feat: create image component

--- src/components/ui/Image.jsx
+++ src/components/ui/Image.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
 
-const Image = () => {
-  return <div>Image</div>;
+import PropTypes from 'prop-types';
+
+const Image = ({
+  src,
+  alt,
+  imageHeight,
+  imageWidth,
+  className,
+  objectFit,
+  loading,
+}) => {
+  return (
+    <>
+      {/* Main image */}
+      <img
+        alt={alt}
+        className={`${imageWidth ? imageWidth : 'w-full'} ${imageHeight ? imageHeight : 'h-full'} ${objectFit ? objectFit : 'object-cover'} ${loading ? 'hidden' : 'block'} ${className}`}
+        loading={loading || 'lazy'}
+        src={src}
+      />
+    </>
+  );
+};
+
+Image.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  skeletonHeight: PropTypes.string,
+  imageWidth: PropTypes.string,
+  imageHeight: PropTypes.string,
+  className: PropTypes.string,
+  objectFit: PropTypes.oneOf([
+    'object-contain',
+    'object-cover',
+    'object-fill',
+    'object-none',
+    'object-scale-down',
+  ]),
+  loading: PropTypes.oneOf(['lazy', 'eager']),
 };
 
 export default Image;
