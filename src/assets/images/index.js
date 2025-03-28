b552d90f70c9fab1dd0940347d0acef5c62fb04c
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-28 18:17:36 +0700

    chore: register images assets

---
+++ src/assets/images/index.js
@@ -0,0 +1,9 @@
+export { default as analisisDanRekomendasi } from './analisis-dan-rekomendasi-cerdas.webp';
+export { default as buatVideoDariUrl } from './buat-video-dari-url.webp';
+export { default as editFleksibelDanCepat } from './edit-fleksibel-dan-cepat.webp';
+export { default as heroImage } from './hero-image.webp';
+export { default as jadwalkanPostingOtomatis } from './jadwalkan-dan-posting-otomatis.webp';
+export { default as step1 } from './step-1-masukkan-url.webp';
+export { default as step2 } from './step-2-konfirmasi-informasi-dan-personalisasi.webp';
+export { default as step3 } from './step-3-generate-dan-dapatkan-hasilnya.webp';
+export { default as templateSiapPakai } from './template-siap-pakai.webp';
