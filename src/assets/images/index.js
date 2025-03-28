dd962257d78c01120642aa9d0af816195863bbe0
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-28 23:00:04 +0700

    chore: add live demo images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,8 +1,11 @@
 export { default as analisisDanRekomendasi } from './analisis-dan-rekomendasi-cerdas.webp';
 export { default as buatVideoDariUrl } from './buat-video-dari-url.webp';
 export { default as editFleksibelDanCepat } from './edit-fleksibel-dan-cepat.webp';
+export { default as emina } from './emina.webp';
+export { default as headphone } from './headphone-bluetooth.webp';
 export { default as heroImage } from './hero-image.webp';
 export { default as jadwalkanPostingOtomatis } from './jadwalkan-dan-posting-otomatis.webp';
+export { default as pashmina } from './pashmina.webp';
 export { default as step1 } from './step-1-masukkan-url.webp';
 export { default as step2 } from './step-2-konfirmasi-informasi-dan-personalisasi.webp';
 export { default as step3 } from './step-3-generate-dan-dapatkan-hasilnya.webp';
