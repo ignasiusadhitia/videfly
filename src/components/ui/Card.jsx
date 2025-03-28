2e85a65f83df9be079b815a52a28a58f581b0dc1
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-29 00:41:42 +0700

    feat:create initial Card component

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
-const Card = () => {
-  return <div>Card</div>;
+import PropTypes from 'prop-types';
+import Image from 'src/components/ui/Image';
+import Typography from 'src/components/ui/Typography';
+
+const Card = ({ imageSource, imageAlt, headingText, bodyText }) => {
+  return (
+    <div>
+      <div>
+        <Image alt={imageAlt} src={imageSource} />
+      </div>
+      <div>
+        <Typography tag="h3">{headingText}</Typography>
+        <Typography>{bodyText}</Typography>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  imageSource: PropTypes.string.isRequired,
+  imageAlt: PropTypes.string.isRequired,
+  headingText: PropTypes.string.isRequired,
+  bodyText: PropTypes.string.isRequired,
 };
 
 export default Card;
