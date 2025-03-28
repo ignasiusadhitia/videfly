c9b29c17a392d8ef9379f53b8712ea2759e7f847
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-29 01:46:56 +0700

    feat: create navbar layout

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
-import { SectionWrapper } from '@ui';
+import data from '@data';
+import * as Icons from '@icons';
+import { Button, Icon, SectionWrapper } from '@ui';
 
 const Navbar = () => {
-  return <SectionWrapper tag="nav">Navbar</SectionWrapper>;
+  const { navbar } = data.sections;
+  const logo = Icons[navbar.logoImgKey];
+
+  return (
+    <SectionWrapper
+      className="flex justify-between items-center py-4"
+      tag="nav"
+    >
+      <Icon IconComponent={logo} width="w-[135.85px]" />
+      <ul className="flex gap-[60px]">
+        {navbar.navLinks.map((navItem) => (
+          <li key={navItem.id}>
+            <a href="#">{navItem.label}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="flex gap-4">
+        {navbar.buttonGroup.map((button) => (
+          <Button key={button.id} label={button.label} />
+        ))}
+      </div>
+    </SectionWrapper>
+  );
 };
 
 export default Navbar;
