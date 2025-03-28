ac2878da5bb4648cf6a35cca3f4a15d8a9d51915
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-29 00:52:22 +0700

    chore: define page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Features, LiveDemo, Navbar, Tutorial, ValuePoint } from '@sections';
+
 function App() {
-  return <h1 className="font-nunito text-5xl">Videfly</h1>;
+  return (
+    <>
+      <header className="mb-[100px]">
+        <Navbar />
+        <LiveDemo />
+      </header>
+      <main className="grid grid-cols-1 gap-[100px]">
+        <Features />
+        <Tutorial />
+        <ValuePoint />
+      </main>
+    </>
+  );
 }
 
 export default App;
