3260376a82cbd39e51cd54ee2ab4aaad69520902
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-28 14:58:02 +0700

    chore: configure tailwindcss and default font

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="font-nunito font-normal">Videfly</h1>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+      nunito: ["Nunito", "sans-serif"],
+    },
+    extend: {
+      fontWeight: {
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+      },
+    },
+  },
+  plugins: [],
+};
