03d26dc9fc1302505bbf2015a376578201ba5175
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-29 01:44:29 +0700

    chore: register icon component

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from './Button';
 export { default as Card } from './Card';
+export { default as Icon } from './Icon';
 export { default as Image } from './Image';
 export { default as Input } from './Input';
 export { default as SectionWrapper } from './SectionWrapper';
