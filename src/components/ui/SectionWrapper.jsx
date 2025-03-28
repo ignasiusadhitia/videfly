c05b28cbaea2008d62469b17d699d74524d354e1
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-29 01:43:24 +0700

    feeat: add tag props

--- src/components/ui/SectionWrapper.jsx
+++ src/components/ui/SectionWrapper.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-const SectionWrapper = ({ tag = 'section', children }) => {
+const SectionWrapper = ({ tag = 'section', className, children }) => {
   // Mapping Tag to HTML tag
   const Tag = tag;
 
-  return <Tag className="container">{children}</Tag>;
+  return <Tag className={`${className} container`}>{children}</Tag>;
 };
 
 SectionWrapper.propTypes = {
   tag: PropTypes.oneOf(['div', 'nav', 'header', 'section', 'footer']),
+  className: PropTypes.string,
   children: PropTypes.node.isRequired,
 };
 
