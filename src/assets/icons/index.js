0930d079eb1b0893ac8036bddf875be65fe8c699
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-28 22:59:09 +0700

    chore: add chevron

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,6 +3,7 @@ export { default as Blibli } from './blibli.svg?react';
 export { default as Calendar } from './calendar-03.svg?.react';
 export { default as CarbonTemplate } from './carbon_template.svg?.react';
 export { default as Chart } from './chart.svg?.react';
+export { default as Chevron } from './chevron.svg?.react';
 export { default as DiamondFilled } from './diamond-filled.svg?.react';
 export { default as Link04 } from './link-04.svg?.react';
 export { default as NextFilled } from './next-filled-svgrepo-com 1.svg?.react';
