b3ec7a676bd490b34e7c8b347bfb46e13b7bd0ee
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-28 18:22:59 +0700

    chore: create initial component template

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as Image } from './Image';
+export { default as Input } from './Input';
+export { default as Select } from './Select';
+export { default as Spinner } from './Spinner';
+export { default as Typography } from './Typography';
