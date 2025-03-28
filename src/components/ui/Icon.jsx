00395cf85a4a02fd4cb90d203b3f3d461c29876a
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-29 00:31:58 +0700

    feat: create icon component

---
+++ src/components/ui/Icon.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+const Icon = ({
+  IconComponent,
+  className,
+  width = 'w-9',
+  height = 'h-9',
+  color = 'text-primary-black',
+  groupHoverColor = 'group-hover:text-primary-green',
+  onClick,
+}) => {
+  const combinedClasses = `${className} ${width} ${height} ${color} ${groupHoverColor} transition-colors duration-500 ease-in-out
+`;
+
+  return <IconComponent className={combinedClasses} onClick={onClick} />;
+};
+
+Icon.propTypes = {
+  IconComponent: PropTypes.elementType.isRequired,
+  className: PropTypes.string,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  color: PropTypes.string,
+  groupHoverColor: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default Icon;
