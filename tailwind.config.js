b3a4e2ba096203274452cde00c00ec67b173c2e7
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-29 07:28:51 +0700

    chore: define color system

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,47 @@ export default {
         semibold: 600,
         bold: 700,
       },
+      colors: {
+        white: {
+          DEFAULT: 'hsla(0, 0%, 100%, 1)',
+          half: 'hsla(0, 0%, 100%, 0.5)',
+        },
+        offWhite: 'hsla(240, 25%, 97%, 1)',
+        purple: {
+          primary: 'hsla(258, 78%, 57%, 1)',
+          secondary: 'hsla(259, 78%, 65%, 1)',
+        },
+        black: 'hsla(0, 0%, 0%, 1)',
+        gray: {
+          200: 'hsla(0, 0%, 54%, 1)',
+          paragraph: 'hsla(0, 0%, 33%, 1)',
+        },
+        red: {
+          dark: 'hsla(4, 49%, 49%, 1)',
+        },
+        disabled: {
+          button: 'hsla(0, 0%, 90%, 1)',
+          label: 'hsla(0, 0%, 69%, 1)',
+        },
+        surface: 'hsla(0, 0%, 96%, 1)',
+        gradients: {
+          purpleLight:
+            'linear-gradient(0deg, #703BE7, #703BE7), linear-gradient(180deg, rgba(255, 255, 255, 0.34) 0%, rgba(255, 255, 255, 0) 100%)',
+          purpleMix: 'linear-gradient(109.89deg, #7F52FF 1.07%, #FB8570 100%)',
+          softGray:
+            'linear-gradient(182.84deg, rgba(245, 245, 249, 0) 2.36%, #F5F5F9 55.82%)',
+          softGrayAlt:
+            'linear-gradient(182.84deg, rgba(245, 245, 249, 0) 2.36%, #F5F5F9 56.12%)',
+          whiteFade:
+            'linear-gradient(270deg, rgba(255, 255, 255, 0) 0%, #FFFFFF 46.27%, #FFFFFF 100%)',
+          purpleSky:
+            'linear-gradient(315.94deg, #F1EDFD -8.05%, #B9AFF7 32.61%, #7261EF 99.96%)',
+          purpleSkyAlt:
+            'linear-gradient(329.07deg, #F1EDFD 24.27%, #B9AFF7 51.81%, #7261EF 69.92%)',
+          purplePeach:
+            'linear-gradient(89.94deg, #703BE7 14.01%, #FFA899 91.74%)',
+        },
+      },
     },
   },
   plugins: [],
