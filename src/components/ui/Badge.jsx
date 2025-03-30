44127f6b29298a8d477f4f4015d80fa4de78f555
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-30 18:22:02 +0700

    chore: remove commented line

--- src/components/ui/Badge.jsx
+++ src/components/ui/Badge.jsx
@@ -9,25 +9,16 @@ import { Icon, Typography } from '@ui';
 const Badge = ({ label }) => {
   const { sectionIconKey } = data.sections;
   const icon = Icons[sectionIconKey];
+
   return (
-    // <div className="relative w-fit flex items-center rounded-[200px] h-7 gap-[7px]  p-[11px] border border-purple-dark">
-    //   <Icon IconComponent={icon} width="w-[13px]" />
-    //   <Typography
-    //     color="text-purple-primary"
-    //     tag="span"
-    //     variant="poppins-regular-14-relaxed"
-    //   >
-    //     {label}
-    //   </Typography>
-    // </div>
     <div
       className="relative w-fit flex items-center rounded-[200px] h-7 gap-[7px] p-[11px] badge-border-gradient"
-      // style={{
-      //   border: '1px solid transparent',
-      //   background:
-      //     'linear-gradient(white, white) padding-box, ' +
-      //     'linear-gradient(109.89deg, #7F52FF 1.07%, #4C3199 100%) border-box',
-      // }}
+      style={{
+        border: '1px solid transparent',
+        background:
+          'linear-gradient(white, white) padding-box, ' +
+          'linear-gradient(109.89deg, #7F52FF 1.07%, #4C3199 100%) border-box',
+      }}
     >
       <Icon IconComponent={icon} width="w-[13px]" />
       <Typography
