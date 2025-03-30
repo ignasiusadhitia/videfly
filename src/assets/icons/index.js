3ec64c06d2b6293b7ea1f53f89e0b7f4aebc9f0a
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-30 23:17:17 +0700

    chore: add loading icon

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -5,6 +5,7 @@ export { default as Chart } from './chart.svg?react';
 export { default as Chevron } from './chevron.svg?react';
 export { default as DiamondFilled } from './diamond-filled.svg?react';
 export { default as Link04 } from './link-04.svg?react';
+export { default as Loading } from './loading.svg?react';
 export { default as NextFilled } from './next-filled-svgrepo-com 1.svg?react';
 export { default as StreamlineAi } from './streamline_ai-edit-spark.svg?react';
 export { default as VideFlyLogo } from './videfly-logo.svg?react';
