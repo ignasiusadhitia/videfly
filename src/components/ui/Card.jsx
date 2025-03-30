6ff4b075422122c5dc469e94fe45ff922dc3d2dd
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-30 07:37:53 +0700

    style: define card style

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-import { Image, Typography } from '@ui';
+import { Icon, Image, Typography } from '@ui';
 
-const Card = ({ imageSource, imageAlt, headingText, bodyText }) => {
+const Card = ({ imageSource, imageAlt, icon, headingText, bodyText }) => {
   return (
-    <div>
-      <div>
+    <div className="grid grid-cols-1 rounded-xl bg-offWhite ">
+      <div className="relative">
         <Image alt={imageAlt} src={imageSource} />
+        <div className="h-[75px] w-full absolute bg-soft-gray blur-sm bottom-0"></div>
       </div>
-      <div>
-        <Typography tag="h3">{headingText}</Typography>
-        <Typography>{bodyText}</Typography>
+      <div className="mt-[3.8px] px-6 pb-[26.2px]">
+        <div className="flex items-center gap-3 mb-[13.8px]">
+          <Icon IconComponent={icon} width="w-6" />
+          <Typography color="text-black" tag="h3" variant="nunito-bold-19">
+            {headingText}
+          </Typography>
+        </div>
+        <Typography variant="poppins-regular-14.4">{bodyText}</Typography>
       </div>
     </div>
   );
@@ -21,6 +27,7 @@ const Card = ({ imageSource, imageAlt, headingText, bodyText }) => {
 Card.propTypes = {
   imageSource: PropTypes.string.isRequired,
   imageAlt: PropTypes.string.isRequired,
+  icon: PropTypes.element.isRequired,
   headingText: PropTypes.string.isRequired,
   bodyText: PropTypes.string.isRequired,
 };
