43f34c922543aca9082db5559827781a8ad76a30
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-30 15:46:46 +0700

    chore: register counter component

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,6 +1,7 @@
 export { default as Badge } from './Badge';
 export { default as Button } from './Button';
 export { default as Card } from './Card';
+export { default as Counter } from './Counter';
 export { default as Icon } from './Icon';
 export { default as Image } from './Image';
 export { default as Input } from './Input';
