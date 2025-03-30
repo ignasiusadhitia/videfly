cf0aef93c5c6bc95e7f6187cf2f64859dacfaf12
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-31 03:29:31 +0700

    style: set mobile view layout, add z-index

--- src/components/ui/Select.jsx
+++ src/components/ui/Select.jsx
@@ -8,7 +8,7 @@ import { Icon, Image, Typography } from '@ui';
 
 // Custom option component
 const Options = ({ items, onSelect }) => (
-  <div className="absolute w-[310px] lg:w-[430px] max-w-[430px] mt-16 rounded-lg py-2 shadow-shadow-custom2 bg-white">
+  <div className="absolute w-[310px] lg:w-[430px] max-w-[430px] lg:mt-16 rounded-lg py-2 shadow-shadow-custom2 bg-white z-30">
     {items.map((item) => (
       <div
         key={item.id}
@@ -63,7 +63,7 @@ const Select = ({
   return (
     <>
       <div
-        className="relative w-[180px] lg:w-full max-w-[430px] h-[50px] py-2 pl-4 pr-2 flex items-center border-[1.55px] border-purple-primary rounded-2xl"
+        className="relative w-full max-w-[310px] lg:max-w-[430px] h-[50px] py-2 pl-4 pr-2 flex items-center border-[1.55px] border-purple-primary rounded-2xl"
         onClick={() => setIsOpen((prev) => !prev)}
       >
         <div className="flex gap-2 items-center flex-grow truncate">
