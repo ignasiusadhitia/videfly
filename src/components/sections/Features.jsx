9ec257819602940b0904631aa7827051cd8bba9d
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2025-03-30 15:10:06 +0700

    feat: create features section

--- src/components/sections/Features.jsx
+++ src/components/sections/Features.jsx
@@ -7,24 +7,24 @@ import { Badge, Card, SectionWrapper, Typography } from '@ui';
 
 const Features = () => {
   const { features } = data.sections;
-  console.log(features);
   return (
-    <SectionWrapper className="grid grid-cols-1 gap-20">
+    <SectionWrapper className="grid grid-cols-1 gap-20 pt-[31px] pb-[60px]">
       <div className="flex flex-col items-center">
         <Badge label={features.name} />
         <Typography
           align="text-center"
-          className="mt-3"
+          className="mt-[33.5px]"
           color="text-black"
           tag="h1"
-          variant="nunito-bold-48"
+          variant="nunito-bold-40"
         >
-          {features.heading}
+          {features.heading}{' '}
           <Typography
             align="text-center"
+            className="mt-4"
             color="text-gradient text-gradient-purple"
             tag="span"
-            variant="nunito-bold-48"
+            variant="nunito-bold-40"
           >
             {features.highlighted}
           </Typography>
